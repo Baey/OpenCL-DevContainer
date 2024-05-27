7f841b286c9a68b6c137c936543b44c32e6ca990
Błażej Szargut <blazejszargut@gmail.com>
2024-05-27 19:44:20 +0200

    Dodane przykłady do Intel OneAPI

---
+++ PlatformCheck/build/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /workspaces/OpenCL-Devcontainer/PlatformCheck
+# Build directory: /workspaces/OpenCL-Devcontainer/PlatformCheck/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
--- PlatformCheck/main.cpp
+++ PlatformCheck/main.cpp
@@ -8,10 +8,20 @@ int main(int, char**){
     std::vector<cl::Platform> platforms;
 	cl::Platform::get(&platforms);
     
-    std::cout << "Dostępne platformy:" << std::endl;
+    std::cout << "Wykryte platformy OpenCL:" << std::endl;
     for (const auto& platform : platforms) {
         std::string platformName;
         platform.getInfo(CL_PLATFORM_NAME, &platformName);
-        std::cout << "- " << platformName << std::endl;
+        std::cout << "|---- " << platformName << std::endl;
+
+        std::vector<cl::Device> devices;
+        platform.getDevices(CL_DEVICE_TYPE_ALL, &devices);
+
+        // std::cout << "Urządzenia dostępne na tej platformie:" << std::endl;
+        for (const auto& device : devices) {
+            std::string deviceName;
+            device.getInfo(CL_DEVICE_NAME, &deviceName);
+            std::cout << "        |---- " << deviceName << std::endl;
+        }
     }
 }
